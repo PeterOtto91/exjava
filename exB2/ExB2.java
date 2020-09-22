f5cb22bcb78a26b311755401a25b0f306eb5d280
PeterOtto91 <peter.otto@telenet.be>
2020-09-22 13:42:53 +0200

    Exercise B2

---
+++ exB2/ExB2.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExB2 {
+	
+	
+	public static void main(String[] args) {
+		
+		String courseLabel;
+		
+		Scanner title = new Scanner(System.in);
+		System.out.println ("Title?");
+		
+		String titleOfCourse = title.nextLine();
+		
+		
+		Scanner numberOfDays = new Scanner(System.in);
+		System.out.println ("Number of days?");
+		
+		int numberOfDaysofCourse = Integer.parseInt(numberOfDays.nextLine());
+		
+		
+		Scanner pricePerDay = new Scanner(System.in);
+		System.out.println ("Price per Day?");
+		
+		double pricePerDayOfCourse = Double.parseDouble(pricePerDay.nextLine());
+		
+		
+		boolean knowledgeRequiredForCourse = true;
+		ArrayList<String> instructorsOfCourse = new ArrayList<String>();
+		
+		instructorsOfCourse.add ("Jan");
+		instructorsOfCourse.add ("Wim");
+		instructorsOfCourse.add ("Dirk");
+		
+		double priceOfCourse = numberOfDaysofCourse * pricePerDayOfCourse;
+		double totalPriceOfCourse = 0;
+		
+		if ((numberOfDaysofCourse > 3) && (knowledgeRequiredForCourse == true)) {
+			totalPriceOfCourse = priceOfCourse;
+		}
+		else {
+			totalPriceOfCourse = priceOfCourse + (priceOfCourse / 100 * 21);
+		}
+		
+		if (totalPriceOfCourse < 500){
+			
+			courseLabel = "Cheap";
+		}
+		else if (totalPriceOfCourse > 1500) {
+			
+			courseLabel = "Expensive";
+		}
+		else {
+			courseLabel = "OK";
+		}
+		
+		
+		System.out.println (titleOfCourse + " " + numberOfDaysofCourse + " " + pricePerDayOfCourse + " " + knowledgeRequiredForCourse);
+		System.out.println ("Number of instructors = " + instructorsOfCourse.size());
+		System.out.println ("Price of Course = " + totalPriceOfCourse);
+		System.out.println ("The price appears to be " + courseLabel);
+		
+	}
+	
+}
