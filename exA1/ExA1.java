1f270ae4cc217d4d266d7af44990317aab3015db
PeterOtto91 <peter.otto@telenet.be>
2020-09-22 10:10:00 +0200

    Exercise 1

---
+++ exA1/ExA1.java
@@ -0,0 +1,13 @@
+public class ExA1 {
+	
+	public static void main(String[] args) {
+		
+		String titleOfCourse = "Java Programming";
+		int numberOfDaysofCourse = 5;
+		double pricePerDayOfCourse = 100.5;
+		boolean knowledgeRequiredForCourse = false;
+		
+		System.out.println (titleOfCourse + " " + numberOfDaysofCourse + " " + pricePerDayOfCourse + " " + knowledgeRequiredForCourse);
+	}
+	
+}
