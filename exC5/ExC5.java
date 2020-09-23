6aa3a422c59379eaca6a217f852f3f5e4fc9239c
PeterOtto91 <peter.otto@telenet.be>
2020-09-23 10:41:00 +0200

    Exercise C5

---
+++ exC5/ExC5.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.NumberFormatException;
+
+public class ExC5 {
+	
+	
+	public static void main(String[] args) {
+		
+		try {
+			
+		String courseLabel;
+		
+			Scanner title = new Scanner(System.in);
+			System.out.println ("Title?");
+		
+			String titleOfCourse = title.nextLine();
+		
+			Scanner numberOfDays = new Scanner(System.in);
+			System.out.println ("Number of days?");
+		
+			int numberOfDaysofCourse = Integer.parseInt(numberOfDays.nextLine());
+		
+		
+		
+			Scanner pricePerDay = new Scanner(System.in);
+			System.out.println ("Price per Day?");
+		
+			double pricePerDayOfCourse = Double.parseDouble(pricePerDay.nextLine());
+		
+		
+			boolean knowledgeRequiredForCourse = true;
+			ArrayList<String> instructorsOfCourse = new ArrayList<String>();
+		
+			instructorsOfCourse.add ("Jan");
+			instructorsOfCourse.add ("Wim");
+			instructorsOfCourse.add ("Dirk");
+		
+			double totalPrice = calculateTotalPrice(numberOfDaysofCourse, pricePerDayOfCourse, knowledgeRequiredForCourse);
+		
+		
+			if (totalPrice < 500){
+			
+				courseLabel = "Cheap";
+			}
+			else if (totalPrice > 1500) {
+			
+				courseLabel = "Expensive";
+			}
+			else {
+				courseLabel = "OK";
+			}
+		
+		
+			printInfo(titleOfCourse, numberOfDaysofCourse, pricePerDayOfCourse, knowledgeRequiredForCourse, instructorsOfCourse.size(), totalPrice, courseLabel);
+		}
+		catch(NumberFormatException e) {
+			
+			System.out.println ("Not Numeric, please run again with correct value.");
+		}
+		
+	}
+	
+	
+	public static void printInfo(String titleIn, int daysIn, double priceIn, boolean knowledgeIn, int instructorsIn, double totalIn, String labelIn) {
+		
+		System.out.println (titleIn + " " + daysIn + " " + priceIn + " " + knowledgeIn);
+		System.out.println ("Number of instructors = " + instructorsIn);
+		System.out.println ("Price of Course = " + totalIn);
+		System.out.println ("The price appears to be " + labelIn);
+		
+	}
+	
+	
+	public static double calculateTotalPrice (int numDays, double priceDay, boolean knowledge) {
+	
+		double priceOfCourse = numDays * priceDay;
+		double totalPriceOfCourse = 0;
+		
+		if ((numDays > 3) && (knowledge)) {
+			totalPriceOfCourse = priceOfCourse;
+		}
+		else {
+			totalPriceOfCourse = priceOfCourse + (priceOfCourse / 100 * 21);
+		}
+		
+		return totalPriceOfCourse;
+	
+	}
+	
+}
